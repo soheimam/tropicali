ca81b00b1a3a64695a29fc6f1f3cc89e39f07540
Soheima Canton <soheima@Soheimas-MacBook-Pro.local>
2018-11-05 12:05:20 +0100

    added gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
 
 gulp.task('default', function(){
-    //place code for task here
+    //run sass "sass --watch assets/css/app.scss:output.css"
+    return gulp.src("assets/css/app.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("output.css"))
 });
